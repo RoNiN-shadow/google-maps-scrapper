7717bea5de1cd4d6f480f3d1d874b083fd36e224
Mark <marktishuk@gmail.com>
2025-10-26 15:32:04 +0100

    feat: refactor saving the data to cloud

--- scrapper/storage.py
+++ scrapper/storage.py
@@ -1,20 +1,15 @@
-from find_email_url import find_email_url
-from scan_local import scan_local
-from typing import Any
 from google.cloud import firestore
 
 
-def save_data():
+class Storage:
+    def save_companies(self, companies: list[dict], email: str):
+        db = firestore.Client()
+        company_ref = db.collection("companies")
 
-    db = firestore.Client()
-    company_ref = db.collection("companies")
-
-    new_companies: list[dict[str, Any]] = scan_local("Krakov")
-
-    for company in new_companies:
-        if company.get("email") is None:
-            get_email = find_email_url(company["website"])
-            company["email"] = get_email
-            doc_ref = company_ref.document(company['name'])
-            if not doc_ref.get().exists:
-                company_ref.document(company['name']).set(company, merge=True)
+        for company in companies:
+            if company.get("email") is None:
+                company["email"] = email
+                doc_ref = company_ref.document(company['name'])
+                if not doc_ref.get().exists:
+                    company_ref.document(company['name']).set(
+                        company, merge=True)
