da244d6056250543366b3b78616c6027fed947d9
Mark <marktishuk@gmail.com>
2025-10-26 15:05:02 +0100

    feat: refactor google maps scaner

--- scrapper/maps.py
+++ scrapper/maps.py
@@ -4,35 +4,36 @@ from dotenv import load_dotenv
 import googlemaps
 
 
-def scan_local(location: str, type: str = 'restaurant') -> list[dict]:
+class Maps:
+    def scan_area(self, location: str, type: str):
+        load_dotenv()
 
-    load_dotenv()
-    gmaps = googlemaps.Client(key=os.getenv(
-        "GOOGLE_MAPS_API_KEY"))
+        gmaps = googlemaps.Client(key=os.getenv(
+            "GOOGLE_MAPS_API_KEY"))
+        geocode_result = gmaps.geocode(location)
 
-    geocode_result = gmaps.geocode(location)
-    latlng = geocode_result[0]["geometry"]["location"]
-    coords = (latlng["lat"], latlng["lng"])
+        latlng = geocode_result[0]["geometry"]["location"]
 
-    restaurants = gmaps.places_nearby(location=coords, radius=5000, type=type)
+        coords = (latlng["lat"], latlng["lng"])
 
-    results = restaurants.get('results', [])
+        restaurants = gmaps.places_nearby(
+            location=coords, radius=5000, type=type)
+        results = restaurants.get('results', [])
 
-    new_companies = []
-    for r in results:
-        pid = r['place_id']
-        details = gmaps.place(place_id=pid, fields=[
-                              'name', 'formatted_address', 'website'])
-        d = details.get('result', {})
-        website = d.get('website')
-        email = d.get('email')
+        new_companies = []
+        for r in results:
+            pid = r['place_id']
+            details = gmaps.place(place_id=pid, fields=[
+                                  'name', 'formatted_address', 'website'])
+            d = details.get('result', {})
+            website = d.get('website')
+            email = d.get('email')
 
-        new_companies.append({
-            'name': d.get('name'),
-            'address': d.get('formatted_address'),
-            'website': website,
-            'emails': list(email),
-        })
-        time.sleep(0.1)
-
-    return new_companies
+            new_companies.append({
+                'name': d.get('name'),
+                'address': d.get('formatted_address'),
+                'website': website,
+                'emails': list(email),
+            })
+            time.sleep(0.1)
+        return new_companies
